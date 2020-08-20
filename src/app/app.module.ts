5f02416caaa7d95535109a04f7bd6e1d17d26d87
aniketdassrit1 <aniketdassrit1@gmail.com>
2020-08-20 22:16:39 +0530

    [dev] added all the required pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { CurrencyConversionComponent } from './currency-conversion/currency-conversion.component';
+import { CurrencyChartComponent } from './currency-chart/currency-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CurrencyConversionComponent,
+    CurrencyChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
